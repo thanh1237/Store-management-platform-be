34254a26bb47c69052a45283bb3bbd3dcd246c8b
Nguyen Hoang Thanh <nhthanh100498@gmail.com>
2021-10-04 15:09:44 +0700

    CORS FIX

--- app.js
+++ app.js
@@ -11,12 +11,27 @@ var indexRouter = require("./routes/index");
 
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+}
+
+
+
 app.use(cors({credentials: true, origin: true}));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(allowCrossDomain);
 
 app.all('/api', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
