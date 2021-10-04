34748753efeec28176898a8d351ff1bbdf5433cf
Nguyen Hoang Thanh <nhthanh100498@gmail.com>
2021-10-04 14:49:21 +0700

    cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(cors({credentials: true, origin: true}));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.all('/', function(req, res, next) {
+app.all('/api', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next()
